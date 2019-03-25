fe96e82b5643e422a1245dc97545d40f0b0e3b16
Liva Rakotoarinia <liva.rakotoarinia@outlook.fr>
2019-03-25 15:31:34 +0100

    SelectedPizza component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,26 @@
 import { Component } from '@angular/core';
-
-export class Pizza{
-  id: number;
-  name: string;
-  price: number;
-}
+import { Pizza } from './models/pizza';
 
 const PIZZAS: Pizza[] = [
-  { id: 1, name: 'Reine', price: 12.99 },
-  { id: 2, name: '4 fromages', price: 13.99 },
-  { id: 3, name: 'Orientale', price: 11.99 },
-  { id: 4, name: 'Cannibale', price: 9.99 }
+  { id: 1, name: 'Reine', price: 12.99, image: 'assets/pizza-1.png' },
+  { id: 2, name: '4 fromages', price: 13.99, image: 'assets/pizza_fresca.jpg' },
+  { id: 3, name: 'Orientale', price: 11.99, image: 'assets/pizza-1.png' },
+  { id: 4, name: 'Cannibale', price: 9.99, image: 'assets/pizza_fresca.jpg' }
 ];
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `
-    <h1>{{ title }} : {{ name }} : </h1>
-    <h2>{{ pizza.name }}</h2>
-    <div>
-      <label>Nom:</label>
-      <input [(ngModel)]="pizza.name">
-      <label>Prix:</label> {{ pizza.price }}
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-3" *ngFor="let pizza of pizzaList">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">{{ pizza.name }}</h5>
-              <p class="card-text">Prix: {{ pizza.price }} &euro;</p>
-            </div>
-          </div>
-        </div>
-      </div>      
-    </div>
-  `,
+  templateUrl: './app.component.html',
+  // template: ``,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'pizzaparty';
-  name = '4 fromages';
-  pizza: Pizza = {
-    id: 1,
-    name: 'Reine',
-    price: 12.99
-  };
+  selectedPizza: Pizza;
   pizzaList = PIZZAS;
+
+  onSelect(pizza: Pizza): void{
+    console.log(pizza);
+    this.selectedPizza = pizza;
+  }
 }
---
+++ src/app/pizza/pizza.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza',
+  templateUrl: './pizza.component.html',
+  styleUrls: ['./pizza.component.scss']
+})
+export class PizzaComponent {
+
+}
